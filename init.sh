8cb8db500fd343b57455a43bbb06fb48d1b14ffc
Tim White <tim@timwhite.io>
2021-08-11 05:04:06 +0200

    add pico-cms php dependencies

--- init.sh
+++ init.sh
@@ -5,14 +5,14 @@ nonRootUsername=tim
 # server updates
 sudo apt update && sudo apt -y upgrade && sudo apt autoremove && apt autoclean && echo "Up to date."
 
-sudo apt install -y php
+sudo apt install -y php php-mbstring php-xml
 sudo apt install -y php apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
 
 sudo groupadd docker
 
