3c9265e44e0ac6ef0da4d82953f2e273feaf2789
Tim White <tim@timwhite.io>
2021-08-11 04:25:47 +0200

    add docker to init

--- init.sh
+++ init.sh
@@ -3,6 +3,15 @@
 # server updates
 sudo apt update && sudo apt -y upgrade && sudo apt autoremove && apt autoclean && echo "Up to date."
 
+sudo apt install -y php
+sudo apt install -y php apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 # UFW firewall
 sudo ufw allow ssh
 sudo ufw allow http
