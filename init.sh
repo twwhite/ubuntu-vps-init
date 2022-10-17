dbe24ebd678eee5213530ec60501d18d3b72b72e
Tim White <tim+git@timwhite.io>
2022-10-16 19:23:21 -0700

    switch for debian/ubuntu docker

--- init.sh
+++ init.sh
@@ -62,6 +62,8 @@ elif [[ "$dist" == *"Ubuntu"* ]]; then
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 fi
 
+sudo apt update
+
 # Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
