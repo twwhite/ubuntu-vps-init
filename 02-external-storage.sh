93ffd69c35a6284a8fdba28a146d3d386320d914
twwhite <tim@timwhite.io>
2023-02-19 20:22:28 -0800

    Add mfsymlinks boot option to allow external symlinking over cifs

--- 02-external-storage.sh
+++ 02-external-storage.sh
@@ -13,6 +13,6 @@ read -p "Enter Hetzner Storage Box Password: " HETZNER_PW
 sudo echo "username=$HETZNER_ID" >> /etc/backup-credentials.txt
 sudo echo "password=$HETZNER_PW" >> /etc/backup-credentials.txt
 
-sudo echo "//$HETZNER_ID.your-storagebox.de/backup$REMOTE_BASE_DIR $EXT_MOUNT_DIR cifs iocharset=utf8,rw,credentials=/etc/backup-credentials.txt,uid=$NON_ROOT_USER,gid=$NON_ROOT_USER,file_mode=0660,dir_mode=0770 0 0" >> /etc/fstab
+sudo echo "//$HETZNER_ID.your-storagebox.de/backup$REMOTE_BASE_DIR $EXT_MOUNT_DIR cifs iocharset=utf8,rw,mfsymlinks,credentials=/etc/backup-credentials.txt,uid=$NON_ROOT_USER,gid=$NON_ROOT_USER,file_mode=0660,dir_mode=0770 0 0" >> /etc/fstab
 
 
