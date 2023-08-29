2807d56e841fd0b7d758bc60a66b489b81e9d2af
twwhite <tim@timwhite.io>
2023-08-28 20:19:09 -0700

    Add letsencrypt root dir

--- 03-applications.sh
+++ 03-applications.sh
@@ -3,3 +3,7 @@
 (crontab -l ; echo "@hourly /home/tim/twio/scripts/recurring/hourly.sh") | crontab -
 (crontab -l ; echo "@daily /home/tim/twio/scripts/recurring/daily.sh") | crontab -
 
+
+# Set up letsencrypt directory with user permissions
+sudo mkdir /letsencrypt
+sudo chown -R tim:tim /letsencrypt
